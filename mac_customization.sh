17dc5688da71fb9d40afe97f15db6d26083fa770
rlseafor <rlseafor@gmail.com>
2020-01-23 13:48:08 -0600

    Simple customization for new macs

---
+++ mac_customization.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR=$HOME/Desktop/screenshots
+echo "Checking for $DIR directory"
+
+# Check for screenshots directory
+if [[ ! -d $DIR ]]
+then
+  echo "$DIR exists, writing defaults"
+  mkdir $DIR
+fi
+
+defaults write com.apple.screencapture location $DIR
