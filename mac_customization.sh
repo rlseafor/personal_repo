f4e42cdfa3954f2aba6557e1739aadb3774a4963
Ryan Seaford <rseaford@chlmd-rseaford1.us.drwholdings.com>
2020-01-24 11:23:54 -0600

    updating mac customization script

--- mac_customization.sh
+++ mac_customization.sh
@@ -12,4 +12,27 @@ fi
 
 defaults write com.apple.screencapture location $DIR
 
-# Install Homebrew
+# Check to see if Homebrew is installed, if not installed
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
+# List of casks to install, then install
+CASKS=(
+  "sublime-text"
+  "iterm2"
+  "atom"
+)
+
+brew cask install $CASKS
+
+# List of packages to install, then install
+BREW_INSTALLS=(
+  "tree"
+  "jq"
+)
+brew install $BREW_INSTALLS
